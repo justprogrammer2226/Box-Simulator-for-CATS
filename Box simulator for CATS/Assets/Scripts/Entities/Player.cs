7a7206a6398f5889eb0fd68b0fd9e32fdedefc55
Vlad Chabanenko <just.programmer2226@gmail.com>
2019-07-29 14:04:46 +0300

    Add rewards for player levels

--- Box simulator for CATS/Assets/Scripts/Entities/Player.cs
+++ Box simulator for CATS/Assets/Scripts/Entities/Player.cs
@@ -21,6 +21,17 @@ class Player : MonoBehaviour
     private void Awake()
     {
         if (instance == null) instance = this;
+
+        // We make a basic check on the correctness of the entered data.
+        // This is necessary in order to avoid unnecessary errors immediately.
+
+        foreach (PlayerLevel playerLevel in playerLevelData.PlayerLevels)
+        {
+            if (playerLevel.Rewards.Select(reward => reward.GameResource).Distinct().Count() != playerLevel.Rewards.Count())
+            {
+                throw new Exception("Rewards should be of different types.");
+            }
+        }
     }
 
     private void Start()
@@ -35,18 +46,23 @@ class Player : MonoBehaviour
 
         // We begin to follow the change in player experience.
         // To be able to change the level of the player.
+
         PlayerGameResource expPlayerGameResource = playerData.PlayerGameResources.Single(playerGameResource => playerGameResource.GameResource == expGameResource);
 
         expPlayerGameResource.OnValueChange += (newValue) =>
         {
             if (playerLevelData.CurrentPlayerLevel != playerLevelData.PlayerLevels.Count && newValue >= playerLevelData.PlayerLevels[playerLevelData.CurrentPlayerLevel].TargetExperience)
             {
+                // Give user rewards.
+                foreach (PlayerGameResource playerGameResource in playerLevelData.PlayerLevels[playerLevelData.CurrentPlayerLevel].Rewards)
+                {
+                    Debug.Log("Level up, reward: " + playerGameResource.GameResource.name);
+                    AdjustGameResource(playerGameResource.GameResource, playerGameResource.Value);
+                }
+
                 int temp = newValue - playerLevelData.PlayerLevels[playerLevelData.CurrentPlayerLevel].TargetExperience;
-
                 playerLevelData.CurrentPlayerLevel++;
-                expPlayerGameResource.Value = temp;
-
-                Debug.Log("LEVEL UP!! Current level is " + playerLevelData.CurrentPlayerLevel);
+                expPlayerGameResource.Value = temp;           
             }  
         };
     }
--- Box simulator for CATS/Assets/Scripts/Systems/PlayerLevels/PlayerLevel.cs
+++ Box simulator for CATS/Assets/Scripts/Systems/PlayerLevels/PlayerLevel.cs
@@ -5,4 +5,7 @@ class PlayerLevel
 {
     [SerializeField] private int _targetExperience;
     public int TargetExperience => _targetExperience;
+
+    [SerializeField] private PlayerGameResource[] _rewards;
+    public PlayerGameResource[] Rewards => _rewards;
 }
